f1eef3d33e05125148d990543288a07f0b985008
root <root@mdeejay.ru>
2017-09-09 17:45:20 +0300

    Import MLTE5 device

--- include/media/msmb_isp.h
+++ include/media/msmb_isp.h
@@ -9,6 +9,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+#ifdef CONFIG_MLTE5_CAMERA
+#include <media/msmb_isp_mlte5.h>
+#else
 #ifndef __MSMB_ISP__
 #define __MSMB_ISP__
 
@@ -639,3 +642,4 @@ struct msm_isp_event_data32 {
 	_IOWR('V', BASE_VIDIOC_PRIVATE+21, struct msm_isp_event_data)
 #endif
 #endif /* __MSMB_ISP__ */
+#endif /* CONFIG_MLTE5_CAMERA */
